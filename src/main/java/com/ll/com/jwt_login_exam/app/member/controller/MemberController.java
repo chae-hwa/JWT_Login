08da84eee9ad717b39616dd9ed76c6e9826ac25c
chae-hwa <ihj0519@naver.com>
2022-10-05 15:36:24 +0900

    MemberController에서 POST/member/login 처리가 가능하도록(응답 헤더)

--- src/main/java/com/ll/com/jwt_login_exam/app/member/controller/MemberController.java
+++ src/main/java/com/ll/com/jwt_login_exam/app/member/controller/MemberController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto){
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response){
+        response.addHeader("Authentication", "JWT토큰");
         return "username : %s, passoword : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
     }
 
