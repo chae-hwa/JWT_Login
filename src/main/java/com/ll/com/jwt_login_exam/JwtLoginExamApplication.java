55fad11e2d8c672a0670036b379e8ed9f88b1b73
chae-hwa <ihj0519@naver.com>
2022-10-05 13:40:17 +0900

    세팅

---
+++ src/main/java/com/ll/com/jwt_login_exam/JwtLoginExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.com.jwt_login_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLoginExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/com/jwt_login_exam/JwtLoginExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.com.jwt_login_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtLoginExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
