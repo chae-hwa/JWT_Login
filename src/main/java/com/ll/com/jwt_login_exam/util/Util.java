a4c4144464ca785800c52521b026d9faf2dac711
chae-hwa <ihj0519@naver.com>
2022-10-06 14:39:07 +0900

    Util.mapOf 구현

--- src/main/java/com/ll/com/jwt_login_exam/util/Util.java
+++ src/main/java/com/ll/com/jwt_login_exam/util/Util.java
@@ -4,8 +4,28 @@ import com.ll.com.jwt_login_exam.app.base.dto.RsData;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Util {
+
+    public static <K, V> Map<K, V> mapOf(Object... args) {
+        Map<K, V> map = new LinkedHashMap<>();
+
+        int size = args.length / 2;
+
+        for (int i = 0; i < size; i++) {
+            int keyIndex = i * 2;
+            int valueIndex = keyIndex + 1;
+
+            K key = (K) args[keyIndex];
+            V value = (V) args[valueIndex];
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
     public static class spring{
 
         public static <T> ResponseEntity<RsData> responseEntityOf(RsData<T> rsData) {
