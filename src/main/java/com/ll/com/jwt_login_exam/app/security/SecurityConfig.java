955d5d7a0e4e848b0fd64899245febdf59c8ba86
chae-hwa <ihj0519@naver.com>
2022-10-07 16:19:21 +0900

    로그인과 회원가입 엔드포인트 제외하고 전부 인증 필요하도록 JwtFilter 추가

--- src/main/java/com/ll/com/jwt_login_exam/app/security/SecurityConfig.java
+++ src/main/java/com/ll/com/jwt_login_exam/app/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ll.com.jwt_login_exam.app.security;
 
+import com.ll.com.jwt_login_exam.app.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -7,21 +8,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws  Exception {
         http
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/member/login", "/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated() // 최소자격 : 로그인
+                )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBasic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
+                ).addFilterBefore(
+                        jwtAuthorizationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 ); // 세션 사용안함
 
         return http.build();
